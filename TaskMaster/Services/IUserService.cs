c191dfcf0d8b9fef53bcacb47f980c28d1325ac0
Nguyen Quang Chien <quangchieens1@gmail.com>
2023-04-14 18:35:52 +0700

    Modify IUserService

--- TaskMaster/Services/IUserService.cs
+++ TaskMaster/Services/IUserService.cs
@@ -1,6 +1,12 @@
+using TaskMaster.Models.Dtos;
+
 namespace TaskMaster.Services;
 
 public interface IUserService
 {
-    
+    Task<IEnumerable<UserDTO>> GetUsers();
+    Task<UserDTO> GetUserById(Guid id);
+    Task<UserDTO> AddUser(UserDTO user);
+    Task<UserDTO> UpdateUser(UserDTO user);
+    Task<UserDTO> DeactivateUser(Guid id);
 }
